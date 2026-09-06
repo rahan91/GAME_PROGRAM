894c46e3951b653a033f05812765a5757254005b
rahan91 <rahaannaeiji@gmail.com>
2026-09-06 13:20:45 +0330

    admin panel: gate on ruruskaado instead of rahan

--- functions/_lib/admin.js
+++ functions/_lib/admin.js
@@ -1,6 +1,6 @@
 import { getUserFromRequest } from './auth.js';
 
-export const ADMIN_USERNAME = 'rahan';
+export const ADMIN_USERNAME = 'ruruskaado';
 
 export async function getAdminUser(env, request) {
   const user = await getUserFromRequest(env, request);
